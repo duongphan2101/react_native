0b670b7dc73420fe05fbf93590a4daddb91b92cd
H82M19\Student <Student>
2024-08-25 14:17:44 +0700

    first-time

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code>src/App.js</code> and save to reload. HEHEHEHHE 
+          <br/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        </p>
       </header>
     </div>
   );
